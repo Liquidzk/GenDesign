937d09f08288a274804a1a63c1cc2f0caeb7594b
Liquidzk <492928615@qq.com>
2023-11-09 12:54:52 +0800

    11.9

--- kangyiqing/pages/mine/jubao.vue
+++ kangyiqing/pages/mine/jubao.vue
@@ -1,61 +1,62 @@
 <template>
-  <div>
-    <video id="video" width="640" height="480" autoplay></video>
-    <button id="snap">Snap Photo</button>
-    <canvas id="canvas" width="640" height="480"></canvas>
-  </div>
+  <view class="content">
+    <button class="snap-button" @click="snapPhoto">Snap a Photo</button>
+    <image v-if="imageBase64" :src="imageBase64" class="photo-preview" />
+  </view>
 </template>
 
 <script>
 export default {
-	data() {
-	    return {
-	      phoneNumber: '1234567890' // 在这里设置已登录用户的手机号码
-	    };
-	  },
-  mounted() {
-    // 设置视频源为用户的摄像头
-    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-      .then(function(stream) {
-        let video = document.getElementById('video');
-        video.srcObject = stream;
-        video.play();
-      }).catch(function(err) {
-        console.log("An error occurred: " + err);
-      });
-
-    // 当点击Snap按钮时，将视频帧画到canvas上
-    let canvas = document.getElementById('canvas');
-    let context = canvas.getContext('2d');
-    let video = document.getElementById('video');
-    document.getElementById("snap").addEventListener("click", function() {
-      context.drawImage(video, 0, 0, 640, 480);
-      let image = canvas.toDataURL('image/jpeg');
-	  var uphone = uni.getStorageSync("phone");
-      // 将图片数据发送到后端
-      this.$http.post('http://localhost:8080/api/face', { image: image , phone: uphone})
-        .then(response => {
-          console.log(response);
-          // handle success
-        }).catch(error => {
-          console.error(error);
-          // handle error
-        });
-    });
-  },
-  onLoad() {
-  	this.getUserinfo();
-  },
-  onShow() {
-      this.getUserinfo();
+  data() {
+    return {
+      imageBase64: ''
+    }
   },
   methods: {
-	  getUserinfo() {
-	  		  var that = this
-	  		  var uphone = uni.getStorageSync("phone");
-	  		  console.log(uphone)
-	  		  
-	  },
+    snapPhoto() {
+      const self = this;
+      uni.chooseImage({
+        count: 1,
+        sourceType: ['camera'],
+        success: function (res) {
+          const tempFilePaths = res.tempFilePaths;
+          uni.getFileSystemManager().readFile({
+            filePath: tempFilePaths[0],
+            encoding: 'base64',
+            success: res => {
+              self.imageBase64 = 'data:image/jpeg;base64,' + res.data;
+              self.uploadPhoto();
+            }
+          });
+        }
+      });
+    },
+    uploadPhoto() {
+      var uphone = uni.getStorageSync("phone");
+      this.$http.post('http://localhost:8080/api/face', { image: this.imageBase64 , phone: uphone})
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
   }
 }
 </script>
+
+<style>
+.content {
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.snap-button {
+  margin-bottom: 20px;
+}
+.photo-preview {
+  width: 200px;
+  height: 200px;
+}
+</style>
